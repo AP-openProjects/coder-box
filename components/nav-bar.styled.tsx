ce18c49f2f2b1aefb891552f26cf646e2fd7d0f8
Capi <eesteban.arce@gmail.com>
2021-09-27 20:36:59 -0300

    cambios e instalacion de dependencias

---
+++ components/nav-bar.styled.tsx
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const Root = styled.div`
+background-color: red;
+height: 50px;
+`
---
+++ ui/botones/index.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+import Button from '@mui/material/Button';
+
+export const OutlinedTextButton = styled(Button).attrs((props) => ({
+}))
+`
+  border-color: red;
+`
+
+//export const OutlinedIconButton
